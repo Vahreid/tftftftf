3cb74f6106c6d52f2cc9af81b6c55af8c874b096
Ekaterina <fhhs567@gmail.com>
2025-01-09 15:00:58 +1000

    тестовый комит

---
+++ proga.py
@@ -0,0 +1,16 @@
+money = [1,2,4,8,16,32,64]
+
+n = int(input('введите нужную вам сумму: '))
+
+bills = []
+n1 = 0
+i = -1
+
+while n1 != n:
+    if n1 + money[i] > n:
+        i -= 1
+    else:
+        n1 += money[i]
+        bills.append(money[i])
+
+print(bills)
